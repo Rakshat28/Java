cdb80a319a8bbc7735519c7ee5b0fe3939a3094f
Rakshat28 <rakshat07ps@gmail.com>
2025-01-03 22:32:00 +0530

    find maximum number of consecutive ones

---
+++ Two-Pointer/485.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int start = 0, max =0;
+        for(int end =0;end<nums.length;end++){
+            if(nums[end] == 0){
+                max = Math.max(max,end - start);
+                start = end + 1;
+            }
+        }
+        return Math.max(max,nums.length - start);
+    }
+}
