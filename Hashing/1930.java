1006f0ce463cb519e1d3e5873f16539a0b8f2150
Rakshat28 <rakshat07ps@gmail.com>
2025-01-04 13:27:52 +0530

    Unique 3 length Palindromic Subsequence

---
+++ Hashing/1930.java
@@ -0,0 +1,29 @@
+package Hashing;
+class Solution {
+    public int countPalindromicSubsequence(String s) {
+        HashMap<Character,Integer> firstIndex = new HashMap<>();
+        HashMap<Character,Integer> lastIndex = new HashMap<>();
+        int length = s.length();
+        for(int i=0;i<length;i++){
+            char ch = s.charAt(i);
+            if(!firstIndex.containsKey(ch)){
+                firstIndex.put(ch,i);
+            }
+            lastIndex.put(ch,i);
+        }
+        int counter = 0;
+        for(char ch : firstIndex.keySet()){
+            int first = firstIndex.get(ch);
+            int last = lastIndex.get(ch);
+            if(first == last){
+                continue;
+            }
+            HashSet<Character> set = new HashSet<>();
+            for(int i=first+1;i<last;i++){
+                set.add(s.charAt(i));
+            }
+            counter +=  set.size();
+        }
+        return counter;
+    }
+}
